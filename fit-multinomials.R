5180908751ec5314e50caaa46745c4f97bc53dec
M-crypto645 <62897582+M-crypto645@users.noreply.github.com>
2023-10-15 16:19:55 +0200

    Update multinomials replication code

---
+++ fit-multinomials.R
@@ -0,0 +1,72 @@
+library(pracma)
+library(gtools)
+library(stats)
+library(corpcor)
+library(gridExtra)
+source("functions/thames.R")
+source("functions/trunc_quantile.R")
+
+d_samples = c(1,20,50,100)
+y_sims = 50
+res_unif_nobound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples),nrow=y_sims)
+res_unif_bound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples),nrow=y_sims)
+res_random_bound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples),nrow=y_sims)
+res_random_nobound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples),nrow=y_sims)
+
+set.seed(1)
+for(c in (1:2)){
+  for(j in (1:length(d_samples))){
+    #browser()
+    d = d_samples[j]
+    K = d+1
+    alpha = rep(1,K)
+    # Since the last parameter is entirely determined by the first K-1 parameters,
+    # the number of dimensions is one lower than the vector
+    n = 400
+    l = 150
+    if(c==1){
+      prob = alpha/K#uniform weights
+    } else {
+      prob = rdirichlet(1,alpha) # 1 fixed probability for each d
+    }
+    for(i in (1:y_sims)){
+      T=10000
+      print(c*i*j)
+      Y = rmultinom(n,size=l,prob=prob) #The data is simulated from the model
+
+      log_prior = function(p) -sum(lgamma(alpha)) + lgamma(sum(alpha)) + sum(log(c(p,(1-sum(p))))*(alpha-1))
+
+      log_likelihood = function(p) sum(apply(Y,2,function(y) dmultinom(y,prob=c(p,1-sum(p)),log=TRUE)))
+
+      post_alpha = apply(Y,1,sum) + matrix(alpha,nrow=1)
+      log_coeff_1 = lgamma(sum(alpha))-sum(lgamma(alpha))
+      log_coeff_2 = n*lgamma(l+1) - sum(lgamma(Y+1))
+      log_coeff_3 = sum(lgamma(post_alpha)) - lgamma(sum(post_alpha))
+      log_marginal_likelihood = log_coeff_1 + log_coeff_2 + log_coeff_3
+
+      param_full = rdirichlet(T,post_alpha)[,(1:d)]
+
+      log_joint = function(param) -(log_prior(param)+log_likelihood(param))
+
+      thames_nobound = thames(params=t(t(param_full)),
+                              lp_func=function(param) -apply(param,1, log_joint))
+      log_thames_nobound = thames_nobound$log_zhat_inv
+
+      thames_bound = thames(params=t(t(param_full)),
+                            lp_func=function(param) -apply(param,1, log_joint),
+                            bound=function(x) (x>0) * (sum(x) <= 1))
+      log_thames_bound = thames_bound$log_zhat_inv
+
+      if(c==1){
+        res_unif_nobound[i,j] = -log_thames_nobound - log_marginal_likelihood
+        res_unif_bound[i,j] = -log_thames_bound - log_marginal_likelihood
+      } else{
+        res_random_nobound[i,j] = -log_thames_nobound - log_marginal_likelihood
+        res_random_bound[i,j] = -log_thames_bound - log_marginal_likelihood
+      }
+    }
+  }
+}
+
+res = cbind(res_unif_bound, res_unif_nobound, res_random_bound, res_random_nobound)
+write.matrix(res,'data/multinomial.csv')
---
+++ plot-multinomials.R
@@ -0,0 +1,40 @@
+library(ggplot2)
+library(ggbeeswarm)
+
+my_theme <- theme_bw() +
+  theme(strip.background = element_rect(fill = "white"), text = element_text(face="bold", size=12),
+  )
+theme_set(my_theme)
+
+res = as.matrix(read.csv('data/multinomial.csv',header=FALSE,sep=''))
+
+d_samples = c(1,20,50,100)
+y_sims = 50
+res_unif_bound = res[,1:length(d_samples)]
+res_unif_nobound = res[,(length(d_samples)+1):(2*length(d_samples))]
+res_random_bound = res[,(2*length(d_samples)+1):(3*length(d_samples))]
+res_random_nobound = res[,(3*length(d_samples)+1):(4*length(d_samples))]
+
+res_unif_nobound = cbind(t(t(c(res_unif_nobound))),rep(d_samples,rep(y_sims,length(d_samples))))
+res_unif_nobound = as.data.frame(res_unif_nobound)
+names(res_unif_nobound) = c("Error","d")
+plot_2 = ggplot(data=res_unif_nobound,aes(x=factor(d),y=Error)) + geom_violin() +
+  geom_beeswarm() + labs(y="Error",x="d") + geom_hline(yintercept = 0, linetype =2, color ="orange") +
+  ylim(-.5,.4) + theme(text=element_text(size=18))
+
+res_random_bound = cbind(t(t(c(res_random_bound))),rep(d_samples,rep(y_sims,length(d_samples))))
+res_random_bound = as.data.frame(res_random_bound)
+names(res_random_bound) = c("Error","d")
+plot_3 = ggplot(data=res_random_bound,aes(x=factor(d),y=Error)) + geom_violin() +
+  geom_beeswarm() + labs(y="Error",x="d") + geom_hline(yintercept = 0, linetype =2, color ="orange") +
+  ylim(-.5,.4) + theme(text=element_text(size=18))
+
+res_random_nobound = cbind(t(t(c(res_random_nobound))),rep(d_samples,rep(y_sims,length(d_samples))))
+res_random_nobound = as.data.frame(res_random_nobound)
+names(res_random_nobound) = c("Error","d")
+plot_4 = ggplot(data=res_random_nobound,aes(x=factor(d),y=Error)) + geom_violin() +
+  geom_beeswarm() + labs(y="Error",x="d") + geom_hline(yintercept = 0, linetype =2, color ="orange") +
+  ylim(-.5,.4) + theme(text=element_text(size=18))
+
+grid.arrange(plot_4, plot_3, plot_2, ncol=3)
+ggsave("plots/simple_multinomial_3_plots.pdf")
