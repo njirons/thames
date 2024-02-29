d39ac1a9ecc314b9068892d860081eae467ff8bf
M-crypto645 <62897582+M-crypto645@users.noreply.github.com>
2024-02-29 09:05:57 +0100

    Update multinomials replication code

--- fit-multinomials.R
+++ fit-multinomials.R
@@ -1,9 +1,9 @@
-library(pracma)
-library(gtools)
-library(stats)
-library(corpcor)
-library(gridExtra)
-library(thames)
+source("multinom-logit_stan.R")
+
+PACKAGES=c("pracma","gtools","stats","corpcor","gridExtra",
+           "thames","bridgesampling","ohenery","MASS",
+           "foreach","parallel", "doParallel","rstan")
+lapply(PACKAGES,require, character.only = TRUE)
 
 d_samples = c(1,20,50,100)
 y_sims = 50
@@ -11,6 +11,14 @@ res_unif_nobound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples)
 res_unif_bound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples),nrow=y_sims)
 res_random_bound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples),nrow=y_sims)
 res_random_nobound = matrix(rep(0,y_sims*length(d_samples)),ncol=length(d_samples),nrow=y_sims)
+
+res_thames_transformed = matrix(ncol=length(d_samples),nrow=y_sims)
+res_bridge = matrix(ncol=length(d_samples),nrow=y_sims)
+res_mcest = matrix(ncol=length(d_samples),nrow=y_sims)
+
+res_systemtimes_bridge = matrix(ncol=length(d_samples),nrow=y_sims)
+res_systemtimes_thames = matrix(ncol=length(d_samples),nrow=y_sims)
+res_systemtimes_mcest = matrix(ncol=length(d_samples),nrow=y_sims)
 
 set.seed(1)
 for(c in (1:2)){
@@ -28,44 +36,153 @@ for(c in (1:2)){
     } else {
       prob = rdirichlet(1,alpha) # 1 fixed probability for each d
     }
-    for(i in (1:y_sims)){
-      T=10000
-      print(c*i*j)
-      Y = rmultinom(n,size=l,prob=prob) #The data is simulated from the model
+    ncores=8
 
-      log_prior = function(p) -sum(lgamma(alpha)) + lgamma(sum(alpha)) + sum(log(c(p,(1-sum(p))))*(alpha-1))
+    cores=detectCores()
+    cl <- makeCluster(min(cores[1]-1,ncores)) #not to overload your computer
+    registerDoParallel(cl)
+    #browser()
+    this <- foreach(i=1:y_sims, .combine=cbind,
+                    .packages=PACKAGES,
+                    .export=c(names(as.list(.GlobalEnv)),ls())) %dopar% {
+                      T=10000
 
-      log_likelihood = function(p) sum(apply(Y,2,function(y) dmultinom(y,prob=c(p,1-sum(p)),log=TRUE)))
+                      print(c*i*j)
+                      Y = rmultinom(n,size=l,prob=prob) #The data is simulated from the model
 
-      post_alpha = apply(Y,1,sum) + matrix(alpha,nrow=1)
-      log_coeff_1 = lgamma(sum(alpha))-sum(lgamma(alpha))
-      log_coeff_2 = n*lgamma(l+1) - sum(lgamma(Y+1))
-      log_coeff_3 = sum(lgamma(post_alpha)) - lgamma(sum(post_alpha))
-      log_marginal_likelihood = log_coeff_1 + log_coeff_2 + log_coeff_3
+                      log_prior = function(p) -sum(lgamma(alpha)) + lgamma(sum(alpha)) + sum(log(c(p,(1-sum(p))))*(alpha-1))
 
-      param_full = rdirichlet(T,post_alpha)[,(1:d)]
+                      log_likelihood = function(p) sum(apply(Y,2,function(y) dmultinom(y,prob=c(p,1-sum(p)),log=TRUE)))
 
-      log_joint = function(param) -(log_prior(param)+log_likelihood(param))
+                      post_alpha = apply(Y,1,sum) + matrix(alpha,nrow=1)
+                      log_coeff_1 = lgamma(sum(alpha))-sum(lgamma(alpha))
+                      log_coeff_2 = n*lgamma(l+1) - sum(lgamma(Y+1))
+                      log_coeff_3 = sum(lgamma(post_alpha)) - lgamma(sum(post_alpha))
+                      log_marginal_likelihood = log_coeff_1 + log_coeff_2 + log_coeff_3
 
-      thames_nobound = thames(params=t(t(param_full)),
-                              lp_func=function(param) -apply(param,1, log_joint))
-      log_thames_nobound = thames_nobound$log_zhat_inv
+                      param_full = rdirichlet(T,post_alpha)[,(1:d)]
 
-      thames_bound = thames(params=t(t(param_full)),
-                            lp_func=function(param) -apply(param,1, log_joint),
-                            bound=function(x) (x>0) * (sum(x) <= 1))
-      log_thames_bound = thames_bound$log_zhat_inv
+                      log_joint = function(param) -(log_prior(param)+log_likelihood(param))
 
-      if(c==1){
-        res_unif_nobound[i,j] = -log_thames_nobound - log_marginal_likelihood
-        res_unif_bound[i,j] = -log_thames_bound - log_marginal_likelihood
-      } else{
-        res_random_nobound[i,j] = -log_thames_nobound - log_marginal_likelihood
-        res_random_bound[i,j] = -log_thames_bound - log_marginal_likelihood
-      }
+                      thames_nobound = thames(params=t(t(param_full)),
+                                              lp_func=function(param) -apply(param,1, log_joint))
+                      log_thames_nobound = thames_nobound$log_zhat_inv
+
+                      thames_bound = thames(params=t(t(param_full)),
+                                            lp_func=function(param) -apply(param,1, log_joint),
+                                            bound=function(x) (x>0) * (sum(x) <= 1))
+                      log_thames_bound = thames_bound$log_zhat_inv
+
+                      # Generating the sample from the transformed posterior and calculating
+                      # THAMES, bridge_sampling and mc_est on it
+                      if(c==1){
+
+                        samples= cbind(1-rowSums(t(t(param_full))),t(t(param_full)))
+                        transformed_samples = t(apply(samples,1,inv_smax))
+                        transformed_samples = t(t(transformed_samples[,-1]))
+                        if(d==1){
+                          colnames(transformed_samples) = c("1")
+                        } else{
+                          colnames(transformed_samples) = c("1",2:dim(transformed_samples)[2])
+                        }
+                        ub = rep(Inf,d)
+                        lb = rep(-Inf,d)
+                        names(lb) <- names(ub) <- colnames(transformed_samples)
+
+                        # Jacobian of the softmax
+                        soft_max_der = function(par) diag(par)-
+                          matrix(rep(par,length(par)),ncol=length(par),nrow=length(par))*t(matrix(rep(par,length(par)),ncol=length(par),nrow=length(par)))
+
+                        # multivariate chain rule component
+                        sum_der = function(par) rbind(diag(length(par)),-1)
+                        #browser()
+                        log_bridge = bridge_sampler(samples=transformed_samples,
+                                                    log_posterior=function(param.row,data) -log_joint(smax(c(-sum(param.row),param.row))[-1])
+                                                    +sum(log(abs(eigen(cbind(diag(length(param.row)),0)%*%soft_max_der(smax(c(-sum(param.row),param.row)))%*%
+                                                                         sum_der(param.row))$values))),# chain rule component
+                                                    data=NULL,ub=ub,lb=lb, silent=TRUE)$logml
+                        log_thames_transformed = thames(params=transformed_samples,
+                                                        lp_func=function(param) apply(param,1,function(param.row)
+                                                          -log_joint(smax(c(-sum(param.row),param.row))[-1])
+                                                          +sum(log(abs(eigen((cbind(diag(length(param.row)),0)%*%soft_max_der(smax(c(-sum(param.row),param.row)))%*%
+                                                                                sum_der(param.row)))$values)))))$log_zhat_inv
+                        prior_sample = rdirichlet(T,alpha)
+                        log_likelihood_prior_sample = sapply(1:T,function(s) log_likelihood(prior_sample[s,-1]))
+
+                        # define naive Monte Carlo estimator
+                        mc_est = function(log_likelihood_prior_sample){
+                          res = list(log_est = log(mean(exp(log_likelihood_prior_sample-max(log_likelihood_prior_sample))))+max(log_likelihood_prior_sample),
+                                     log_sd=sd(exp(log_likelihood_prior_sample-max(log_likelihood_prior_sample))))
+                          return(res)
+                        }
+
+                        log_mc_est = mc_est(log_likelihood_prior_sample)$log_est
+
+                        # Precompute lps and calculate computation times on a stan fit object
+                        lps = apply(transformed_samples ,1,function(param.row)
+                          -log_joint(smax(c(-sum(param.row),param.row))[-1])
+                          +sum(log(abs(eigen((soft_max_der(smax(c(-sum(param.row),param.row)))%*%
+                                                sum_der(param.row))[-1,])$values))))
+
+                        lmm_full_test <- stan_model(model_code = lmm_test)
+                        standata = list(d=K,n_samp=n,y=t(Y),a=alpha)
+                        lmm_full_fit <- sampling(lmm_full_test,
+                                                 data = standata,
+                                                 iter = T,
+                                                 chains=1,
+                                                 warmup=0,
+                                                 algorithm="Fixed_param",
+                                                 pars = c('theta_tilde'))
+
+                        # Set samples and lps to exact simulation from posterior
+                        #browser()
+                        for(i in 1:d){
+                          lmm_full_fit@sim$samples[[1]][[i]] = transformed_samples[,i]
+                        }
+                        #browser()
+                        #lmm_full_fit@sim$samples[[1]]$`theta_tilde[1]`=transformed_samples
+                        lmm_full_fit@sim$samples[[1]]$lp__=lps[lmm_full_fit@sim$permutation[[1]]]
+                        new_mod <- stan(model_code = lmm_test, data = standata, chains = 0)
+
+                        # Calculate system times
+                        systemtimes_bridge = system.time(bridge_sampler(lmm_full_fit,new_mod,silent = TRUE)$logml)[3]
+                        systemtimes_thames = system.time(thames(params=transformed_samples,lps=lps)$log_zhat_inv)[3]
+                        systemtimes_mcest = system.time(mc_est(log_likelihood_prior_sample)$log_est)[3]
+                      }
+
+                      if(c==1){
+                        c(-log_thames_nobound-log_marginal_likelihood,
+                          -log_thames_bound-log_marginal_likelihood,
+                          log_mc_est-log_marginal_likelihood,
+                          log_bridge-log_marginal_likelihood,
+                          -log_thames_transformed-log_marginal_likelihood,
+                          systemtimes_bridge, systemtimes_thames, systemtimes_mcest)
+                      } else{
+                        c(-log_thames_nobound, -log_thames_bound)-log_marginal_likelihood
+                      }
+                    }
+    stopCluster(cl)
+    #browser()
+    if(c==1){
+      res_unif_nobound[,j] = this[1,]#res_thames_thames_nobound[,j] = this[1,]#-log_thames_transformed - log_marginal_likelihood
+      res_unif_bound[,j] = this[2,]#res_bridge[,j] = this[2,]#log_bridge - log_marginal_likelihood
+      res_mcest[,j] = this[3,]
+      res_bridge[,j] = this[4,]#-log_thames_nobound - log_marginal_likelihood
+      res_thames_transformed[,j] = this[5,]#-log_thames_bound - log_marginal_likelihood
+      res_systemtimes_bridge[,j] = this[6,]
+      res_systemtimes_thames[,j] = this[7,]
+      res_systemtimes_mcest[,j] = this[8,]
+    } else{
+      res_random_nobound[,j] = this[1,]#-log_thames_nobound - log_marginal_likelihood
+      res_random_bound[,j] = this[2,]#-log_thames_bound - log_marginal_likelihood
     }
+    #browser()
+
   }
 }
 
-res = cbind(res_unif_bound, res_unif_nobound, res_random_bound, res_random_nobound)
+res = cbind(res_unif_nobound, res_unif_bound,
+            res_random_nobound, res_random_bound,
+            res_mcest, res_bridge, res_thames_transformed,
+            res_systemtimes_bridge, res_systemtimes_thames, res_systemtimes_mcest)
 write.matrix(res,'data/multinomial.csv')
--- plot-multinomials.R
+++ plot-multinomials.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(ggbeeswarm)
+library(gridExtra)
 
 my_theme <- theme_bw() +
   theme(strip.background = element_rect(fill = "white"), text = element_text(face="bold", size=12),
@@ -10,10 +11,36 @@ res = as.matrix(read.csv('data/multinomial.csv',header=FALSE,sep=''))
 
 d_samples = c(1,20,50,100)
 y_sims = 50
-res_unif_bound = res[,1:length(d_samples)]
-res_unif_nobound = res[,(length(d_samples)+1):(2*length(d_samples))]
-res_random_bound = res[,(2*length(d_samples)+1):(3*length(d_samples))]
-res_random_nobound = res[,(3*length(d_samples)+1):(4*length(d_samples))]
+res_unif_nobound = res[,1:length(d_samples)]
+res_unif_bound = res[,(length(d_samples)+1):(2*length(d_samples))]
+res_random_nobound = res[,(2*length(d_samples)+1):(3*length(d_samples))]
+res_random_bound = res[,(3*length(d_samples)+1):(4*length(d_samples))]
+res_mcest = res[,(4*length(d_samples)+1):(5*length(d_samples))]
+res_bridge = res[,(5*length(d_samples)+1):(6*length(d_samples))]
+res_thames_transformed = res[,(6*length(d_samples)+1):(7*length(d_samples))]
+res_systemtimes_bridge = res[,(7*length(d_samples)+1):(8*length(d_samples))]
+res_systemtimes_thames = res[,(8*length(d_samples)+1):(9*length(d_samples))]
+res_systemtimes_mcest = res[,(9*length(d_samples)+1):(10*length(d_samples))]
+
+# Calculate summary measures of estimators
+
+# means
+cbind(round(apply(res_mcest,2,function(x) mean(abs(x))),4),
+round(apply(res_bridge,2,function(x) mean(abs(x))),4),
+round(apply(res_thames_transformed,2,function(x) mean(abs(x))),4))
+
+#standard deviations
+cbind(round(apply(res_mcest,2,function(x) sd(x)),4),
+round(apply(res_bridge,2,function(x) sd(x)),4),
+round(apply(res_thames_transformed,2,function(x) sd(x)),4))
+
+# Calculate summary measures of systemtimes
+cbind(round(apply(res_systemtimes_mcest,2,function(x) mean(abs(x))),4),
+round(apply(res_systemtimes_bridge,2,function(x) mean(abs(x))),4),
+round(apply(res_systemtimes_thames,2,function(x) mean(abs(x))),4))
+
+# multiplicative differences
+round(apply(res_systemtimes_bridge,2,function(x) mean(abs(x)))/apply(res_systemtimes_thames,2,function(x) mean(abs(x))))
 
 res_unif_nobound = cbind(t(t(c(res_unif_nobound))),rep(d_samples,rep(y_sims,length(d_samples))))
 res_unif_nobound = as.data.frame(res_unif_nobound)
@@ -33,6 +60,13 @@ res_random_nobound = cbind(t(t(c(res_random_nobound))),rep(d_samples,rep(y_sims,
 res_random_nobound = as.data.frame(res_random_nobound)
 names(res_random_nobound) = c("Error","d")
 plot_4 = ggplot(data=res_random_nobound,aes(x=factor(d),y=Error)) + geom_violin() +
+  geom_beeswarm() + labs(y="Error",x="d") + geom_hline(yintercept = 0, linetype =2, color ="orange") +
+  theme(text=element_text(size=18))
+
+res_bridge = cbind(t(t(c(res_bridge))),rep(d_samples,rep(y_sims,length(d_samples))))
+res_bridge = as.data.frame(res_bridge)
+names(res_bridge) = c("Error","d")
+plot_1 = ggplot(data=res_bridge,aes(x=factor(d),y=Error)) + geom_violin() +
   geom_beeswarm() + labs(y="Error",x="d") + geom_hline(yintercept = 0, linetype =2, color ="orange") +
   theme(text=element_text(size=18))
 
